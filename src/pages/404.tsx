267c1a149662923497fb80bc82faa80e3ec47cf5
taese0ng <taese0ng@naver.com>
2023-03-26 15:32:07 +0900

    fix: 404페이지 수정

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,12 +1,13 @@
-export default function Custom404() {
-  return null;
-}
+import { useGetWindow } from "@/hooks";
+import Link from "next/link";
 
-export const getStaticProps = () => {
-  return {
-    redirect: {
-      destination: "/",
-      permanent: false,
-    },
-  };
-};
+export default function Custom404() {
+  const { isMobile } = useGetWindow();
+
+  return (
+    <div>
+      <h1>잘못된 경로입니다.</h1>
+      <Link href={isMobile ? "/mobile" : "/"}>Home</Link>
+    </div>
+  );
+}
